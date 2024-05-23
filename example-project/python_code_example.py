a78888811a38dfe92f02d26ecfa0e50f95682279
j-s-ashley <jashley6@vols.utk.edu>
2024-05-23 13:36:55 -0400

    :white_check_mark: Successful test of example

---
+++ example-project/python_code_example.py
@@ -0,0 +1,9 @@
+from example import square
+
+square2 = square(2)
+square3 = square(3)
+square11 = square(11)
+
+print("2 squared is", square2)
+print("3 squared is", square3)
+print("11 squared is", square11)
