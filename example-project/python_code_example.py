83472a7acc8a49793ccf45154b1b5df294cceef7
j-s-ashley <jashley6@vols.utk.edu>
2024-05-23 13:45:30 -0400

    :memo: Reminder: if (script name = library name): :boom:

--- example-project/python_code_example.py
+++ example-project/python_code_example.py
@@ -1,5 +1,14 @@
 from example import square
 
+'''
+Just going to leave a reminder here:
+    If you use the same name for your 
+    python script as a python library
+    (especially one that you call IN 
+    that script), you're gonna have a
+    bad time.
+'''
+
 square2 = square(2)
 square3 = square(3)
 square11 = square(11)
