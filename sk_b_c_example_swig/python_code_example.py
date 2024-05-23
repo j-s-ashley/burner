236c60e6ffde1f4be9897611d0ea24d7ca4698f1
j-s-ashley <jashley6@vols.utk.edu>
2024-05-23 15:14:29 -0400

    Successful build/install test

---
+++ sk_b_c_example_swig/python_code_example.py
@@ -0,0 +1,18 @@
+from example import square
+
+'''
+Just going to leave a reminder here:
+    If you use the same name for your 
+    python script as a python library
+    (especially one that you call IN 
+    that script), you're gonna have a
+    bad time.
+'''
+
+square2 = square(2)
+square3 = square(3)
+square11 = square(11)
+
+print("2 squared is", square2)
+print("3 squared is", square3)
+print("11 squared is", square11)
